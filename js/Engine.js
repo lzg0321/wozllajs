554e59a3b687b56fcf632a193a97c995ac250aef
greek-mythology <117292775@qq.com>
2013-07-24 11:22:59 +0800

    Add Engine and Time

---
+++ js/Engine.js
@@ -0,0 +1,68 @@
+/**
+ * this singleton controls main loop of game.
+ *
+ *
+ *
+ */
+WozllaJS.define('Engine', function($) {
+
+    var requestAnimationFrame =
+            window.requestAnimationFrame ||
+            window.webkitRequestAnimationFrame ||
+            window.mozRequestAnimationFrame ||
+            window.oRequestAnimationFrame ||
+            function(frameCall, intervalTime) {
+                setTimeout(frameCall, intervalTime);
+            };
+
+    var displayCanvas;
+    var running = true;
+    var loopListeners = [];
+
+    return {
+
+        init : function(width, height, canvas) {
+            displayCanvas = canvas;
+            canvas.width = width;
+            canvas.height = height;
+
+            loopListeners = [];
+            $.Time.reset();
+            this.addMainLoopListener(function() {
+                $.Time.update();
+            });
+        },
+
+        addMainLoopListener : function(listener) {
+            loopListeners.push(listener);
+        },
+
+        startMainLoop : function() {
+            running = true;
+
+            function frame() {
+                if(!running) return;
+
+                var i, len =loopListeners.length;
+                for(i=0; i<len; i++) {
+                    loopListeners[i]();
+                }
+
+                requestAnimationFrame(frame, 1000/100);
+
+            }
+
+            requestAnimationFrame(frame, 1000/100);
+        },
+
+        stopMainLoop : function() {
+            running = false;
+        },
+
+        getDisplayCanvas : function() {
+            return displayCanvas;
+        }
+
+    }
+
+});
