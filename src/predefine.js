0a2b30b64c33d2057ecef0c8b0f3e682bcde10bf
xiaoming <117292775@qq.com>
2013-08-17 16:48:23 +0800

    add predefine.js for fix browser compatibility

---
+++ src/predefine.js
@@ -0,0 +1,8 @@
+window.requestAnimationFrame =
+    window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.oRequestAnimationFrame ||
+    function(frameCall, intervalTime) {
+        setTimeout(frameCall, intervalTime);
+    };
