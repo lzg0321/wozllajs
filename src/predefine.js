a3f8bb29379ca0180fcfb8a108f7c80a19b12528
xiaoming <117292775@qq.com>
2013-08-18 15:54:50 +0800

    add Array.remove

--- src/predefine.js
+++ src/predefine.js
@@ -9,3 +9,14 @@ window.requestAnimationFrame =
     };
 
 // Array
+
+Array.prototype.remove = function(obj) {
+    var i, len;
+    for(i=0, len=this.length; i<len; i++) {
+        if(this[i] === obj) {
+            this.splice(i, 1);
+            return i;
+        }
+    }
+    return -1;
+};
