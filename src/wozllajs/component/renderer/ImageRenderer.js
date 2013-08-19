df8b81ba1e7bcc5a1b45208570a47737b87e144e
xiaoming <117292775@qq.com>
2013-08-19 10:11:20 +0800

    add ImageRenderer

---
+++ src/wozllajs/component/renderer/ImageRenderer.js
@@ -0,0 +1,36 @@
+Class.define('wozlla.component.renderer.ImageRenderer', {
+
+    extend : 'wozlla.Component',
+
+    image : null,
+
+    src : null,
+
+    renderWidth : null,
+    renderHeight : null,
+
+    initialize : function() {
+        this.callParent(arguments);
+    },
+
+    init : function() {
+        this.image = this.getResourceById(this.src);
+    },
+
+    draw : function(context) {
+        if(this.image) {
+            if(this.renderWidth && this.renderHeight) {
+                context.drawImage(this.image, 0, 0, this.renderWidth, this.renderHeight);
+            } else {
+                context.drawImage(this.image, 0, 0);
+            }
+        }
+    },
+
+    _getResources : function(res) {
+        if(this.src) {
+            res.push(this.src);
+        }
+    }
+
+});
