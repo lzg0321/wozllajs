f34be5980a4031d53985c21ca1f2ef82c85d44fc
xiaoming <117292775@qq.com>
2013-08-25 20:34:38 +0800

    updated

--- src/predefine.js
+++ src/predefine.js
@@ -1,22 +1,28 @@
-// global vars
-window.requestAnimationFrame =
-    window.requestAnimationFrame ||
-    window.webkitRequestAnimationFrame ||
-    window.mozRequestAnimationFrame ||
-    window.oRequestAnimationFrame ||
-    function(frameCall, intervalTime) {
-        setTimeout(frameCall, intervalTime);
+(function() {
+
+
+    // global vars
+    window.requestAnimationFrame =
+        window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        window.oRequestAnimationFrame ||
+        function(frameCall, intervalTime) {
+            setTimeout(frameCall, intervalTime);
+        };
+
+    // Array
+
+    Array.prototype.remove = function(obj) {
+        var i, len;
+        for(i=0, len=this.length; i<len; i++) {
+            if(this[i] === obj) {
+                this.splice(i, 1);
+                return i;
+            }
+        }
+        return -1;
     };
 
-// Array
 
-Array.prototype.remove = function(obj) {
-    var i, len;
-    for(i=0, len=this.length; i<len; i++) {
-        if(this[i] === obj) {
-            this.splice(i, 1);
-            return i;
-        }
-    }
-    return -1;
-};
+});
