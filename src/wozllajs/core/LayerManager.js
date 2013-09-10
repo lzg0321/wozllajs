14884c7b30f823cd772071fcb0ce06bc66160133
xiaoming <117292775@qq.com>
2013-09-10 23:23:42 +0800

    updated

--- src/wozllajs/core/LayerManager.js
+++ src/wozllajs/core/LayerManager.js
@@ -3,12 +3,23 @@ this.wozllajs = this.wozllajs || {};
 this.wozllajs.LayerManager = (function() {
 
     var layerObjects = new wozllajs.Array2D(); // TODO 固定array2D
-    var layers = null;
+    var layers = {};
+    var layerList = [];
 
     return {
 
         init : function(theLayers) {
+            var name;
             layers = theLayers;
+            for(name in theLayers) {
+                layerList.push({
+                    name : name,
+                    z : theLayers[name]
+                });
+            }
+            layerList.sort(function(a, b) {
+                return b.z - a.z;
+            });
         },
 
         appendTo : function(layerId, gameObject) {
@@ -24,7 +35,11 @@ this.wozllajs.LayerManager = (function() {
         },
 
         getLayerObjects : function(layerId) {
-            return [].concat(layerObjects.get(layerId));
+            return layerObjects.get(layerId);
+        },
+
+        getSortedLayerList : function() {
+            return layerList;
         }
     }
 
