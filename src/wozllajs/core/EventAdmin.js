1cc945bc6cd8eda6e35a356278404376ee12a358
xiaoming <117292775@qq.com>
2013-09-10 23:23:35 +0800

    add proxy to listener

--- src/wozllajs/core/EventAdmin.js
+++ src/wozllajs/core/EventAdmin.js
@@ -4,29 +4,42 @@ this.wozllajs.EventAdmin = (function() {
 
     var eventDispatcher = new wozllajs.EventDispatcher();
 
+    function getProxyKey(type) {
+        return '_wozllajs_proxy_' + type;
+    }
+
     return {
 
-        on : function(type, scope, listener) {
-            var proxy = listener['_wozllajs_proxy_' + type] = wozllajs.proxy(listener, scope);
-
+        on : function(type, gameObject, listener, scope) {
+            var proxyKey = getProxyKey(type);
+            var proxy = wozllajs.proxy(listener, scope);
+            listener[proxyKey] = listener[proxyKey] || [];
+            listener[proxyKey].push(proxy);
             if(wozllajs.Touch.isTouchEvent(type)) {
-                wozllajs.Touch.on(type, scope, listener);
-                return;
+                wozllajs.Touch.on(type, gameObject, proxy);
+            } else {
+                eventDispatcher.addEventListener(type, proxy);
             }
-
-            eventDispatcher.addEventListener(type, proxy);
         },
 
-        off : function(type, scope, listener) {
-            var proxy = listener['_wozllajs_proxy_' + type];
-            listener['_wozllajs_proxy_' + type] = false;
-            if(proxy) {
-                if(wozllajs.Touch.isTouchEvent(type)) {
-                    wozllajs.Touch.off(type, scope, listener);
-                    return;
+        off : function(type, gameObject, listener) {
+            var proxy, i, len;
+            var proxyKey = getProxyKey(type);
+            var proxies = listener[proxyKey];
+            var isTouch = wozllajs.Touch.isTouchEvent(type);
+            if(proxies) {
+                for(i=0,len=proxies.length; i<len; i++) {
+                    proxy = proxies[i];
+                    if(proxy) {
+                        if(isTouch) {
+                            wozllajs.Touch.off(type, gameObject, proxy);
+                        } else {
+                            eventDispatcher.removeEventListener(type, proxy);
+                        }
+                    }
                 }
-                eventDispatcher.removeEventListener(type, proxy);
             }
+            delete listener[proxyKey];
         },
 
         notify : function(type, params) {
