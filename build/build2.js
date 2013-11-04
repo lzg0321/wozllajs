856da285d18503b88a57b754da36d55f91880a6e
xiaoming <117292775@qq.com>
2013-11-04 11:17:12 +0800

    updated

--- build/build2.js
+++ build/build2.js
@@ -1,5 +1,11 @@
 ({
     baseUrl: "./../source",
     name: "wozlla",
-    out: "./../libs/wozllajs-v2-min.js"
+    out: "./../libs/wozllajs-v2-min.js",
+    onBuildWrite : function(moduleName, path, contents) {
+        if(moduleName === 'wozlla') {
+            return contents.replace("'wozlla'", "'wozllajs'");
+        }
+        return contents;
+    }
 })
